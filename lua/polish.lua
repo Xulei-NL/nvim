ab7fceb010b791ee7b143f6310538995b875c9c2
Xulei Liu <xuliu@deloitte.nl>
2024-09-13 08:41:37 +0200

    update my astronvim config

--- lua/polish.lua
+++ lua/polish.lua
@@ -15,8 +15,6 @@
 --   },
 -- }
 
-vim.wo.relativenumber = false
-vim.o.wrap = true
 vim.keymap.set("n", "<A-j>", ":m .+1<CR>==") -- move line up(n)
 vim.keymap.set("n", "<A-k>", ":m .-2<CR>==") -- move line down(n)
 vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv") -- move line up(v)
