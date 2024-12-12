df9276aed1a211be422e984e3e03accf14ce5b7d
Xulei Liu <xuliu@deloitte.nl>
2024-12-12 20:18:05 +0100

    switch to lazyvim

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,38 +1,44 @@
--- Customize Treesitter
-
----@type LazySpec
 return {
-	"nvim-treesitter/nvim-treesitter",
-	opts = {
-		ensure_installed = {
-			"bash",
-			"json",
-			"jsonc",
-			"lua",
-			"markdown",
-			"python",
-			"terraform",
-			"yaml",
-			"vimdoc",
-			"vim",
-			"c",
-			"query",
-			"markdown_inline",
-			"diff",
-
-			"gitattributes",
-			"gitcommit",
-			"gitignore",
-			"git_config",
-			"git_rebase",
-
-			"go",
-			"gomod",
-			"gosum",
-			"gotmpl",
-			"gowork",
-			"helm",
-			-- add more arguments for adding more treesitter parsers
-		},
-	},
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "c",
+        "diff",
+        "dockerfile",
+        "gitattributes",
+        "git_config",
+        "git_rebase",
+        "gitcommit",
+        "gitignore",
+        "go",
+        "gomod",
+        "gosum",
+        "gotmpl",
+        "gowork",
+        "helm",
+        "json",
+        "jsonc",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "proto",
+        "python",
+        "query",
+        "terraform",
+        "vim",
+        "vimdoc",
+        "yaml",
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    enabled = false,
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    enabled = false,
+  },
 }
