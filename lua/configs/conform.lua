95059292bb668ca35056e033392f2291c878a731
Xulei Liu <xuliu@deloitte.nl>
2025-02-15 22:02:37 +0100

    update

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,20 +1,19 @@
 local options = {
-  formatters_by_ft = {
-    lua = { "stylua" },
-    go = { "gofmt" },
-    hcl = { "packer_fmt" },
-    terraform = { "terraform_fmt" },
-    tf = { "terraform_fmt" },
-    ["terraform-vars"] = { "terraform_fmt" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
-  },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		go = { "gofmt" },
+		hcl = { "packer_fmt" },
+		terraform = { "terraform_fmt" },
+		tf = { "terraform_fmt" },
+		["terraform-vars"] = { "terraform_fmt" },
+		haskell = { "hindent", "fourmolu" },
+	},
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+	format_on_save = {
+		-- These options will be passed to conform.format()
+		timeout_ms = 1000,
+		lsp_fallback = true,
+	},
 }
 
 return options
